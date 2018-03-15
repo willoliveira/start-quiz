41a32963dd91b5320d872cecdb60bd6c0c637be5
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2018-03-15 08:33:56 -0300

    Deletando arquivo desnecessario

--- src/app/pages/game/components/pipe-test.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeTestPipe } from './pipe-test.pipe';
-
-describe('PipeTestPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeTestPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
